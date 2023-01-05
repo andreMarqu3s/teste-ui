28daa0362e4a08b913d8a570eb11376a2b8aa8c5
andre_marques <markues@protonmail.com>
2023-01-05 12:07:33 -0300

    teste endereco de entrega com page objects e arquivo de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -29,7 +29,7 @@ describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
     );
   });
 
-  it("Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados", () => {
+  it("Deve fazer cadastro de endereço de faturamento com sucesso - usando arquivo de dados", () => {
     enderecoPage.editarEnderecoFaturamento(
       dadosEndereco[0].nome,
       dadosEndereco[0].sobrenome,
@@ -42,6 +42,24 @@ describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
       dadosEndereco[0].cep,
       dadosEndereco[0].telefone,
       dadosEndereco[0].email
+    );
+    cy.get(".woocommerce-message").should(
+      "contain",
+      "Endereço alterado com sucesso."
+    );
+  });
+
+  it("Deve fazer cadastro endereço de entrega com sucesso - usando arquivo de dados", () => {
+    enderecoPage.editarEnderecoEntrega(
+      dadosEndereco[1].nome,
+      dadosEndereco[1].sobrenome,
+      dadosEndereco[1].empresa,
+      dadosEndereco[1].pais,
+      dadosEndereco[1].endereco,
+      dadosEndereco[1].numero,
+      dadosEndereco[1].cidade,
+      dadosEndereco[1].estado,
+      dadosEndereco[1].cep,
     );
     cy.get(".woocommerce-message").should(
       "contain",
